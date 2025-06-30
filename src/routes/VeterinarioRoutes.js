861649351ef0eb31bd9d7745cf5f57e8d20a3e0f
ignacio <ignacioigartua15@gmail.com>
2025-06-30 16:00:40 -0300

    correccion de error pide validacion en veterinario

--- src/routes/VeterinarioRoutes.js
+++ src/routes/VeterinarioRoutes.js
@@ -10,10 +10,10 @@ class VeterinarioRouter {
 
   startRoutes() {
     this.router.get("/veterinarios", this.controller.getVeterinarios);
-    this.router.post("/veterinarios", authMiddleware.verifyToken, this.controller.postVeterinario);
+    this.router.post("/veterinarios", this.controller.postVeterinario);
     this.router.patch("/veterinarios/:id", this.controller.patchVeterinario);
     this.router.put("/veterinarios/:id", this.controller.putVeterinario);
-    this.router.delete("/veterinarios/:id", authMiddleware.verifyToken, this.controller.deleteVeterinario);
+    this.router.delete("/veterinarios/:id", this.controller.deleteVeterinario);
     return this.router;
   }
 }
